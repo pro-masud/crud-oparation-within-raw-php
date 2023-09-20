6a3155b9bc5accb379c9c6cdf8c0b968cb045418
pro-mannan <promasudbd@gmail.com>
2023-09-20 23:03:32 +0600

    CRUD Oparation setup complete

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- normalize file -->
+    <link rel="stylesheet" href="./css/normalize.css">
+    <!-- milligram file -->
+    <link rel="stylesheet" href="./css/milligram.min.css">
+    <!-- CSS Main File -->
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+<body>
+
+<div class="forms">
+        <div class="container">
+            <div class="row">
+                <div class="column column-70 column-offset-30">
+                    <div style="text-align: center;" class="student-admin">
+                        <h1>Student Information</h1>
+                        <?php 
+                            include_once"./nav/nav.php";
+                        ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+</body>
+</html>
