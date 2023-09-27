20f1adeba2890fa6e85366bb4a892c13e31aaca5
pro-mannan <promasudbd@gmail.com>
2023-09-27 15:05:17 +0600

    students edit oparation complete

--- inc/functions.php
+++ inc/functions.php
@@ -1,103 +1,179 @@
 <?php 
-/**
- * create a database path and database name here 
- * */ 
 
-define("DB", "C:/xampp/htdocs/crud/DB/database.txt");
+define("DB", "C:/xampp/htdocs/final-pro/DB/database.txt");
 
+// seed function by default data sent to database
 
 
-/**
- * create a students data for seed name
- * */ 
-
- function seed(){
-    $Data = [
-      
+function seed(){
+    $data = [
+        [
+            "id"    => 1,
+            "fname"     => "Masud",
+            "lname"     => "Rana",
+            "roll"      => 2,
+        ],
+        [
+            "id"    => 2,
+            "fname"     => "Khalid",
+            "lname"     => "Hasan",
+            "roll"      => 6,
+        ],
+        [
+            "id"    => 3,
+            "fname"     => "Sofik",
+            "lname"     => "Khan",
+            "roll"      => 7,
+        ],
+        [
+            "id"        => 4,
+            "fname"     => "Maruf",
+            "lname"     => "Khan",
+            "roll"      => 9,
+        ],
+        [
+            "id"        => 5,
+            "fname"     => "Masud",
+            "lname"     => "Rana",
+            "roll"      => 10,
+        ],
     ];
 
-    $serializeData = serialize($Data);
+    $students = serialize($data);
 
-    file_put_contents(DB, $serializeData, LOCK_EX);
- }
+    file_put_contents(DB, $students, LOCK_EX);
+}
+
 
 
 /**
- * create a all student report here now
  * 
- * */
+ * get student data to database
+ * 
+ * */ 
 
- function studentReports(){
-    $serializeData = file_get_contents(DB);
-    $students = unserialize($serializeData);
-
+ function getStudents(){
+    $unserialize = file_get_contents(DB);
+    $students = unserialize($unserialize);
     ?>
+
         <table>
             <thead>
                 <tr>
                     <th>Id</th>
-                    <th>Photo</th>
                     <th>Name</th>
-                    <th>Age</th>
-                    <th>Class</th>
                     <th>Roll</th>
                     <th>Action</th>
                 </tr>
             </thead>
             <tbody>
                 <?php foreach($students as $student): ?>
-                    <tr>
-                        <td><?php echo $student['id']; ?></td>
-                        <td><img style="width: 30px; height=30px;" src="../uploads/<?php echo $student['photo']; ?>"></td>
-                        <td><?php echo $student['fname']. " " . $student['lname']; ?></td>
-                        <td><?php echo $student['age']; ?></td>
-                        <td><?php echo $student['class']; ?></td>
-                        <td><?php echo $student['roll']; ?></td>
-                        <td><a style="color: green;" href="index.php?task=edite&id=<?php echo $student['id']; ?>">Edite</a> | <a style="color: red;" href="index.php?task=delete&id=<?php echo $student['id']; ?>">Delete</a></td>
-                    </tr>
-                    <?php endforeach; ?>
+                <tr>
+                <td><?php echo $student['id']; ?></td>
+                    <td><?php echo $student['fname'] . " " .  $student['lname'] ?></td>
+                    <td><?php echo $student['roll']; ?></td>
+                    <td><a style="color: green;" href="index.php?task=edit&id=<?php echo $student['id']; ?>">Edit</a> | <a style="color: red;" href="index.php?task=delete&id=<?php echo $student['id']; ?>">Delete</a></td>
+                </tr>
+                <?php endforeach; ?>
             </tbody>
         </table>
-    <?php 
 
-
+    <?php
  }
 
 
- /**
- * 
- * new student data send to database
- * 
- * */
 
- function addStudentData($fname, $lname, $age, $class, $roll, $photo){
+
+
+/**
+ * 
+ * new student add and send to database
+ * 
+*/
+
+function addNewStudents($fname, $lname, $roll){
     $founts = false;
-    $serializeData = file_get_contents(DB);
-    $students = unserialize($serializeData);
+    $unserialize = file_get_contents(DB);
+    $students = unserialize($unserialize);
+   
+
     foreach($students as $_student){
         if($_student['roll'] == $roll){
             $founts = true;
             break;
         }
     }
+
     if(!$founts){
-        $newId = count($students);
+        $newId = count($students) + 1;
         $newStudent = [
-            "id" => $newId,
+            "id"    => $newId,
             "fname" => $fname,
             "lname" => $lname,
-            "age"   => $age,
-            "class" => $class,
             "roll"  => $roll,
-            "photo"  => $photo['name'],
         ];
-
+    
         array_push($students, $newStudent);
-
-        $serializeData = serialize($students);
-        file_put_contents(DB, $serializeData, LOCK_EX);
+    
+    
+        $serialize = serialize($students);
+    
+        file_put_contents(DB, $serialize, LOCK_EX);
         return true;
     }
 
     return false;
+
+}
+
+
+
+/**
+ * student update information 
+ * */ 
+
+ function editStudent($id){
+    $unserialize = file_get_contents(DB);
+    $students = unserialize($unserialize);
+   
+
+    foreach($students as $student){
+        if($student['id'] == $id){
+            return $student;
+        }
+    }
+    return false;
+ }
+
+
+
+/**
+ * 
+ * update student information
+ * */  
+
+ function upDateStudentInfo($id, $fname, $lname, $roll){
+    $founts = false;
+    $unserialize = file_get_contents(DB);
+    $students = unserialize($unserialize);
+
+    foreach($students as $_student){
+        if($_student['roll'] == $roll && $_student['id'] != $id){
+            $founts = true;
+            break;
+        }
+    }
+    
+    if(!$founts){
+        $students[$id-1]['fname']    = $fname;
+        $students[$id-1]['lname']    = $lname;
+        $students[$id-1]['roll']    = $roll;
+
+        $serialize = serialize($students);
+    
+        file_put_contents(DB, $serialize, LOCK_EX);
+        return true;
+    }
+
+    return false;    
  }
--- index.php
+++ index.php
@@ -1,59 +1,51 @@
 <?php 
-/**
- * project all files required here
- * */ 
-require "./inc/functions.php";
-$info = '';
-$task = $_GET['task'] ?? 'report';
-$error = $_GET['error'] ?? '0';
-if("seed" == $task){
-    seed();
-    $info = "seeding is complete Now";
-}
+    require_once"./inc/functions.php";
+    $info = '';
+    $task = $_GET['task'] ?? 'report';
+    $error = $_GET['error'] ?? '0';
 
-
-
-/**
- * 
- * new student data upload to database 
- * 
- * */ 
-
- $fname = "";
- $lname = "";
- $age = "";
- $class = "";
- $roll = "";
- $photo = "";
-
- if(isset($_POST['submit'])){
-    $fname = $_POST['fname'];
-    $lname = $_POST['lname'];
-    $age = $_POST['age'];
-    $class = $_POST['class'];
-    $roll = $_POST['roll'];
-    $photo = $_FILES['photo'];
-
-
-
-    
-    if($fname != "" && $lname != "" && $age != "" && $class != "" && $roll != "" && $photo != ""){
-        
-       $result =  addStudentData($fname, $lname, $age, $class, $roll, $photo);
-    
-        move_uploaded_file($photo['tmp_name'], "./uploads/" . $photo['name']);
-
-
-        $info = "Data Send Successfuly";
-        if($result){
-            header('location: index.php?task=report');
-        }else{
-            $error = 1;
-        }
-    }else{
-        $info = "Must field Not Empty!!!";
+    if("seed" == $task){
+        seed();
+        $info = "seeding is complete";
     }
- }
+
+
+    $fname = "";
+    $lname = "";
+    $roll = "";
+
+    // data send to database 
+    if(isset($_POST['submit'])){
+        $fname = $_POST['fname'];
+        $lname = $_POST['lname'];
+       $roll = $_POST['roll'];
+       
+       $id = "";
+       if(isset($_POST['id'])){
+            $id = $id = $_POST['id'];
+       }
+
+        if($id){
+           $result = upDateStudentInfo($id, $fname, $lname, $roll);
+           if($result){
+                header('location: index.php?task=report');
+            }else{
+                $error = 1;
+            }
+        }else{
+            if($fname != "" && $lname != "" && $roll != "" ){
+                $result =  addNewStudents($fname, $lname, $roll);
+                 if($result){
+                     header('location: index.php?task=report');
+                 }else{
+                     $error = 1;
+                 }
+             }else{
+                 $info = "Data Not sent For Database";
+             }
+        }
+    }
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -75,8 +67,8 @@ if("seed" == $task){
                 <div class="column column-70 column-offset-30">
                     <div style="text-align: center;" class="student-admin">
                         <h1>Student Information</h1>
-                        <?php 
-                            include_once"./nav/nav.php";
+                        <?php
+                            include_once "./nav/nav.php";
                         ?>
                         <p>
                             <?php echo $info; ?>
@@ -84,49 +76,65 @@ if("seed" == $task){
                     </div>
                 </div>
             </div>
+            <div class="row">
+                <?php if("report" == $task):?>
+                    <div class="column column-70 column-offset-30">
+                        <?php print_r(getStudents()); ?>
+                    </div>
+                <?php endif; ?>
+            </div>
 
-            
             <?php if("1" == $error): ?>
-            <div class="row">
-                <div class="column column-70 column-offset-30">
-                   <blockquote>
-                        <p>Dublicate Roll Number</p>
-                   </blockquote>
+                <div class="row">
+                    <blockquote>
+                        This Roll Is Dublicate
+                    </blockquote>
                 </div>
-            </div>
             <?php endif; ?>
-
-
-            <?php if("report" == $task): ?>
-            <div class="row">
-                <div class="column column-70 column-offset-30">
-                    <?php studentReports(); ?>
-                </div>
-            </div>
-            <?php endif; ?>
-
 
             <?php if("add" == $task): ?>
-            <div class="row">
+                <div class="row">
                 <div class="column column-50 column-offset-20">
-                   <form class="student_add_form" action="index.php?task=add" method="POST"  enctype="multipart/form-data">
+                   <form class="student_add_form" action="index.php?task=add" method="POST">
                         <label for="fname">First Name</label>
                         <input type="text" id="fname" name="fname" value="<?php echo $fname; ?>">
                         <label for="lname">Last Name</label>
                         <input type="text" id="lname" name="lname" value="<?php echo $lname; ?>">
-                        <label for="age">Age</label>
-                        <input type="text" id="age" name="age" value="<?php echo $age; ?>">
-                        <label for="class">Class</label>
-                        <input type="text" id="class" name="class" value="<?php echo $class; ?>">
                         <label for="roll">Roll</label>
                         <input type="text" id="roll" name="roll" value="<?php echo $roll; ?>">
-                        <label for="photo">Photo</label>
-                        <input type="file" id="photo" name="photo" value="<?php echo $photo; ?>">
                         <button type="submit" name="submit">Save Data</button>
                    </form>
                 </div>
             </div>
             <?php endif; ?>
+
+            <!-- student edite data here now -->
+            <?php if("edit" == $task): 
+               
+                 $id = $_GET['id'];
+                
+                 $student = editStudent($id);
+
+                 if($student):
+                
+                ?>
+                <div class="row">
+                <div class="column column-50 column-offset-20">
+                   <form class="student_add_form" action="index.php?task=report" method="POST">
+                        <input type="hidden" value="<?php echo $id ?>" name="id">
+                        <label for="fname">First Name</label>
+                        <input type="text" id="fname" name="fname" value="<?php echo $student['fname']; ?>">
+                        <label for="lname">Last Name</label>
+                        <input type="text" id="lname" name="lname" value="<?php echo $student['lname']; ?>">
+                        <label for="roll">Roll</label>
+                        <input type="text" id="roll" name="roll" value="<?php echo  $student['roll']; ?>">
+                        <button type="submit" name="submit">Update Data</button>
+                   </form>
+                </div>
+            </div>
+            <?php
+                endif;
+            endif; ?>
         </div>
     </div>
     
